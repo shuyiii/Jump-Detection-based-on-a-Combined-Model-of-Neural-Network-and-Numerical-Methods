cc10705712d1e93eb1ddd9919d4fe08d189640b4
shuyiii <34970502+shuyiii@users.noreply.github.com>
2019-09-22 23:12:03 -0400

    Update mix-11_norm.py

--- 1d/mix-11_norm.py
+++ 1d/mix-11_norm.py
@@ -4,7 +4,7 @@ from keras import backend as K
 from keras.engine.topology import Layer
 from keras.models import Sequential
 from keras.layers import Lambda,Dense,BatchNormalization,Flatten,Reshape,Dropout, Activation,Convolution2D,GlobalAveragePooling1D
-from keras.optimizers import SGD, Adam
+from keras.optimizers import Adam
 import numpy as np
 from scipy.io import loadmat
 import matplotlib.pyplot as plt
@@ -34,11 +34,11 @@ y_train=y_train.astype(np.float64)
 for i in range(1000000):
     for j in range(201):
         if y_train[i,j]==2:
-            y_train[i,j]=-1           
+            y_train[i,j]=-1
+#no need shuffle data
 ########################################################################################################
 def root_mean_squared_error(y_pred,y_true):
         return K.sqrt(K.mean(K.square(y_pred - y_true), axis=-1)) 
-    
                    
 model = Sequential()
 model.add(Convolution2D(24, (1,2),strides=(1,1),input_shape=(1,202,1)))
@@ -52,13 +52,11 @@ model.add(Activation('relu'))
 model.add(Convolution2D(24, (1,2),strides=(1,2),input_shape=(1,202,1)))
 model.add(Activation('relu'))
 model.add(Flatten())
-model.add(Dense(201))#relu or not?  should restrict each to 0 and 1
+model.add(Dense(201))
 
-
+filename="/users/PAS1263/osu8085/0.25_1_mil/ker2/ker2_-1_and_1_normalize.hdf5"
+checkpoint = ModelCheckpoint(filename, monitor='val_loss', verbose=0, save_best_only=True,mode='max')
+callbacks_list = [checkpoint]
 adam=Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
-model.compile(optimizer = "adam", loss = root_mean_squared_error, 
-              metrics =["accuracy"])
-history = model.fit(x_train, y_train, batch_size=20000, epochs=300, verbose=0)
-########################################################################################################
-filepath="/users/PAS1263/osu8085/0.25_1_mil/ker2/"
-model.save_weights(filepath+'ker2_-1_and_1_normalize.h5')
+model.compile(optimizer = "adam", loss = root_mean_squared_error, metrics =["accuracy"])
+model.fit(x_train, y_train,validation_split=0.1，batch_size=20000, epochs=300, callbacks=callbacks_list,verbose=0)
