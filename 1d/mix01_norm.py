03f14b46d4c65300f9d11b687c317f80705f9537
shuyiii <34970502+shuyiii@users.noreply.github.com>
2019-09-22 23:13:15 -0400

    Update mix01_norm.py

--- 1d/mix01_norm.py
+++ 1d/mix01_norm.py
@@ -46,11 +46,10 @@ model.add(Activation('relu'))
 model.add(Flatten())
 model.add(Dense(201))#relu or not?  should restrict each to 0 and 1
 
-
+filename="/users/PAS1263/osu8085/0.25_1_mil/ker2/ker2_0_and_1_normalize.hdf5"
+checkpoint = ModelCheckpoint(filename, monitor='val_loss', verbose=0, save_best_only=True,mode='max')
+callbacks_list = [checkpoint]
 adam=Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
 model.compile(optimizer = "adam", loss = root_mean_squared_error, 
               metrics =["accuracy"])
-history = model.fit(x_train, y_train, batch_size=20000, epochs=300, verbose=0)
-########################################################################################################
-filepath="/users/PAS1263/osu8085/0.25_1_mil/ker2/"
-model.save_weights(filepath+'ker2_0_and_1_normalize.h5')
+model.fit(x_train, y_train,validation_split=0.1，batch_size=20000, epochs=300, callbacks=callbacks_list,verbose=0)
