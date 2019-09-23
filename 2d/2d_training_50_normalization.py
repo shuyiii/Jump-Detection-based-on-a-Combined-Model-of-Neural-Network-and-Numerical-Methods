ec3a9258a3345283ef7eca5b8af1e2581f8fb29c
shuyiii <34970502+shuyiii@users.noreply.github.com>
2019-09-22 23:30:16 -0400

    Update 2d_training_50_normalization.py

--- 2d/2d_training_50_normalization.py
+++ 2d/2d_training_50_normalization.py
@@ -54,11 +54,9 @@ model.add(Flatten())
 model.add(Dense(2500))
 
 adam=Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
-model.compile(optimizer = "adam", loss = root_mean_squared_error, 
-              metrics =["accuracy"])
-
+model.compile(optimizer = "adam", loss = root_mean_squared_error, metrics =["accuracy"])
 filepath="/users/PAS1263/osu8085/2D_models/new_train/"
-model.load_weights(filepath+'model_weights_50_50_normalization_each.h5')
-history = model.fit(train_data,train_label, batch_size=1000, epochs=30, verbose=0)
-
-model.save_weights(filepath+'model_weights_50_50_normalization_each.h5')
+filename=filepath+"/model_weights_50_50_normalization_each.hdf5"
+checkpoint = ModelCheckpoint(filename, monitor='val_loss', verbose=0, save_best_only=True, mode='max')
+callbacks_list = [checkpoint]
+model.fit(train_data,train_label, validation_split=0.1，callbacks=callbacks_list,batch_size=1000, epochs=100, verbose=0)
